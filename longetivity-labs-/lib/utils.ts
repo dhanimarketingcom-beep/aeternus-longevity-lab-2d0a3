abafab58378a19b15f77bfb5f92dec1af58eb44e
Dhani Marketing Services <dhanimarketing.com@gmail.com>
2026-03-30 13:51:38 +0530

    Refactor getProjectRoot to eliminate fs dependency
    Removed fs dependency and updated getProjectRoot function to return process.cwd() directly.

--- longetivity-labs-/lib/utils.ts
+++ longetivity-labs-/lib/utils.ts
@@ -1,6 +1,5 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import fs from 'node:fs'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -10,5 +9,5 @@ export function getProjectRoot(): string {
   if (typeof window !== 'undefined') {
     return '/'
   }
-  return fs.realpathSync(process.cwd())
+      return process.cwd()
 }
